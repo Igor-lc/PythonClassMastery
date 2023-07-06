65977fd68bead99f777714ac78a4e4d27551549e
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-07-07 02:17:57 +0300

    Update decorator_.py

--- decorator_.py
+++ decorator_.py
@@ -15,3 +15,21 @@ def div(a, b):
 print(div(10, 5))
 print(div(10, 5))
 print(div(10, 5))
+
+
+def register_call(func):
+    def w(*args):
+        w.c += 1
+        print("reg_call =", w.c)
+        return func(*args)
+    w.c = 0
+    return w
+
+
+@register_call
+def test(a, b):
+    return a * b
+
+
+print(test(1, 2))
+print(test(2, 3))
