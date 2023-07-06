3d66abf970a86db8c963d2d5c1e1b7e6643e9cd1
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-07-06 19:21:25 +0300

    Add files via upload

--- decorator_.py
+++ decorator_.py
@@ -1,13 +1,16 @@
-def second(func):
-    print("second")
-    def first(*args):
-        print("first")
-        return f"{func(*args)}, {args}"
-    return first
+def third(*args2):
+    print("third")
+    def second(func):
+        print("second")
+        def first(*args):
+            print("first")
+            return f"{func(*args)}, {func(*args2)}"
+        return first
+    return second
 
-@second
+@third(10, 10)
 def div(a, b):
-    return a * b
+    return a + b
 
 print(div(10, 5))
 print(div(10, 5))
