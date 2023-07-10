a21c0814efead1de8e52c2f8fd52dfe4d0d89b09
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-07-10 11:14:46 +0300

    Add files via upload

--- functors.py
+++ functors.py
@@ -4,10 +4,7 @@ Create a functor called AddConstantFunctor that adds a constant value to a given
 Task 2: StringLengthCheckFunctor
 Implement a functor called StringLengthCheckFunctor that checks whether a given string has more than 5 characters. The functor should return true if the string is longer than 5 characters and false otherwise.
 
-Task 3: CelsiusToFahrenheitFunctor
-Implement a functor called CelsiusToFahrenheitFunctor that converts a temperature in Celsius to Fahrenheit. The functor should take a temperature in Celsius as an argument and return the equivalent temperature in Fahrenheit.
-
-Task 4: IntegerSortingFunctor
+Task 3: IntegerSortingFunctor
 Create a functor called IntegerSortingFunctor that sorts a list of integers in ascending order. You can use any sorting algorithm of your choice (e.g., bubble sort or insertion sort) to implement the functor.'''
 
 class AddConstantFunctor:
@@ -36,9 +33,15 @@ obj = StringLengthCheckFunctor()
 print(obj('123456'))
 
 
-class CelsiusToFahrenheitFunctor:
-    def __call__(self, celsius):
-        return celsius * 9 / 5 + 32
 
-obj = CelsiusToFahrenheitFunctor()
-print(obj(20))
+from time import time
+class IntegerSortingFunctor:
+    def __call__(self, numbers):
+        start = time()
+        sorted_numbers = sorted(numbers)
+        print(time() - start)
+        return sorted_numbers
+
+
+obj = IntegerSortingFunctor()
+print(obj([7, 8, 7, 8, 6, 9, 9, 7, 7, 8]))
